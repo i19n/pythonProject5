2df045200a45a8f6d382f0ad7e12d28cf1af929f
Иван <sigaga511@gmail.com>
2022-07-06 15:40:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+int_list=[1,2,3,5]
+char_list=['a','c','z','x']
+empty_list=[]
+
+print("Числа",int_list)
+print("Символы",char_list)
+print("Пустой список",empty_list)
+
+list_from_range=list(range(5))
+print("из диапазона:",list_from_range)
+
+lisr_from_string=list("строка")
+print("из строки:",lisr_from_string)
